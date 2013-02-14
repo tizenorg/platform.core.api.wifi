3989854a046d46c4264c93386db5e83a273614cb
HyungKyu Song <hk76.song@samsung.com>
2013-02-14 21:43:47 +0900

    Tizen 2.0 Release

---
+++ include/net_wifi_private.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2012-2013 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+#ifndef __NET_CONNECTION_PRIVATE_H__        /* To prevent inclusion of a header file twice */
+#define __NET_CONNECTION_PRIVATE_H__
+
+#include <dlog.h>
+#include <network-cm-intf.h>
+#include <network-wifi-intf.h>
+#include "wifi.h"
+
+#undef LOG_TAG
+#define LOG_TAG "CAPI_NETWORK_WIFI"
+
+#define WIFI_INFO	1
+#define WIFI_ERROR	2
+#define WIFI_WARN	3
+
+#define WIFI_LOG(log_level, format, args...) \
+	do { \
+		switch (log_level) { \
+		case WIFI_ERROR: \
+			LOGE(format, ## args); \
+			break; \
+		case WIFI_WARN: \
+			LOGW(format, ## args); \
+			break; \
+		default: \
+			LOGI(format, ## args); \
+		} \
+	} while(0)
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+bool _wifi_libnet_init(void);
+bool _wifi_libnet_deinit(void);
+int _wifi_activate(wifi_activated_cb callback, void* user_data);
+int _wifi_deactivate(wifi_deactivated_cb callback, void* user_data);
+
+bool _wifi_libnet_check_ap_validity(wifi_ap_h ap_h);
+void _wifi_libnet_add_to_ap_list(wifi_ap_h ap_h);
+void _wifi_libnet_remove_from_ap_list(wifi_ap_h ap_h);
+bool _wifi_libnet_check_profile_name_validity(const char *profile_name);
+
+bool _wifi_libnet_get_wifi_device_state(wifi_device_state_e *device_state);
+bool _wifi_libnet_get_wifi_state(wifi_connection_state_e* connection_state);
+int _wifi_libnet_get_intf_name(char** name);
+int _wifi_libnet_scan_request(wifi_scan_finished_cb callback, void* user_data);
+int _wifi_libnet_get_connected_profile(wifi_ap_h *ap);
+bool _wifi_libnet_foreach_found_aps(wifi_found_ap_cb callback, void *user_data);
+
+int _wifi_libnet_open_profile(wifi_ap_h ap_h, wifi_connected_cb callback, void* user_data);
+int _wifi_libnet_close_profile(wifi_ap_h ap_h, wifi_disconnected_cb callback, void* user_data);
+int _wifi_libnet_connect_with_wps(wifi_ap_h ap, wifi_connected_cb callback, void* user_data);
+int _wifi_libnet_forget_ap(wifi_ap_h ap);
+
+int _wifi_set_power_on_off_cb(wifi_device_state_changed_cb callback, void *user_data);
+int _wifi_unset_power_on_off_cb(void);
+int _wifi_set_background_scan_cb(wifi_scan_finished_cb callback, void *user_data);
+int _wifi_unset_background_scan_cb(void);
+int _wifi_set_connection_state_cb(wifi_connection_state_changed_cb callback, void *user_data);
+int _wifi_unset_connection_state_cb();
+
+int _wifi_update_ap_info(net_profile_info_t *ap_info);
+wifi_connection_state_e _wifi_convert_to_ap_state(net_state_type_t state);
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
---
+++ packaging/capi-network-wifi.spec
@@ -0,0 +1,59 @@
+Name:       capi-network-wifi
+Summary:    Network Wi-Fi library in TIZEN C API
+Version:    0.1.2_14
+Release:    1
+Group:      System/Network
+License:    Apache License Version 2.0
+Source0:    %{name}-%{version}.tar.gz
+BuildRequires:  cmake
+BuildRequires:  pkgconfig(dlog)
+BuildRequires:  pkgconfig(glib-2.0)
+BuildRequires:  pkgconfig(vconf)
+BuildRequires:  pkgconfig(capi-base-common)
+BuildRequires:  pkgconfig(network)
+Requires(post): /sbin/ldconfig
+Requires(postun): /sbin/ldconfig
+
+%description
+Network Wi-Fi library in Tizen C API
+
+%package devel
+Summary:  Network Wi-Fi library in Tizen C API (Development)
+Group:    System/Network
+Requires: %{name} = %{version}-%{release}
+
+%description devel
+Network Wi-Fi library in Tizen C API (Development)
+
+%prep
+%setup -q
+
+
+%build
+MAJORVER=`echo %{version} | awk 'BEGIN {FS="."}{print $1}'`
+cmake . -DCMAKE_INSTALL_PREFIX=/usr -DFULLVER=%{version} -DMAJORVER=${MAJORVER}
+
+make %{?_smp_mflags}
+
+
+%install
+%make_install
+
+#License
+mkdir -p %{buildroot}%{_datadir}/license
+cp LICENSE.APLv2 %{buildroot}%{_datadir}/license/capi-network-wifi
+
+%post -p /sbin/ldconfig
+
+%postun -p /sbin/ldconfig
+
+
+%files
+%manifest capi-network-wifi.manifest
+%attr(644,-,-) %{_libdir}/libcapi-network-wifi.so.*
+%{_datadir}/license/capi-network-wifi
+
+%files devel
+%{_includedir}/network/*.h
+%{_libdir}/pkgconfig/*.pc
+%{_libdir}/libcapi-network-wifi.so
