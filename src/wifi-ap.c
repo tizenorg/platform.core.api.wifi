c527c3ec117ba5d2c98b578a3c724fed1377cfbe
Maneesh Jain <maneesh.jain@samsung.com>
2014-10-15 16:29:28 +0530

    [wifi]: Fix:Return error in case proxy type Direct
    Change-Id: If19af85380c887809a70251896600e8134e2705b
    Signed-off-by: Maneesh Jain <maneesh.jain@samsung.com>

--- src/wifi-ap.c
+++ src/wifi-ap.c
@@ -677,6 +677,8 @@ EXPORT_API int wifi_ap_set_proxy_address(wifi_ap_h ap,
 		err = connman_service_set_proxy_config(service, &proxy_config);
 		g_free(proxy_config.url);
 	}
+	else
+		return WIFI_ERROR_INVALID_PARAMETER;
 
 	if (err != CONNMAN_LIB_ERR_NONE)
 		return _wifi_connman_lib_error2wifi_error(err);
